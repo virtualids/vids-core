40c158373426b594f486eeb61550e0532e46c859
Brody Carty <brodycarty@gmail.com>
2023-05-29 12:56:29 -0500

    add electron and index.html

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+
+app.whenReady().then(() => {
+
+    // create a window
+    const myWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+})
