337d708654e63014c1d44d3824067f7052729f6c
Brody Carty <brodycarty@gmail.com>
2023-07-12 13:27:17 -0500

    add facility folders, modal changes

--- IDSRcab/modalcontrol.js
+++ IDSRcab/modalcontrol.js
@@ -1,19 +1,120 @@
-var modal = document.getElementById("settingsModal");
 
-var btn = document.getElementById("twrBtnSettings");
 
-var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
+// settings modal
+
+var settingsModal = document.getElementById("settingsModal");
+var settingsbtn = document.getElementById("twrBtnSettings");
+var settingsModalClose = document.getElementsByClassName("settingsModalClose")[0];
+
+settingsbtn.onclick = function() {
+    settingsModal.style.display = "block";
 }
 
-span.onclick = function() {
-    modal.style.display = "none";
+settingsModalClose.onclick = function() {
+    settingsModal.style.display = "none";
 }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-    modal.style.display = "none";
-    }
+// wx modal
+
+var wxModal = document.getElementById("wxModal");
+var wxbtn = document.getElementById("twrBtnWX");
+var wxModalClose = document.getElementsByClassName("wxModalClose")[0];
+
+wxbtn.onclick = function() {
+    wxModal.style.display = "block";
 }
+
+wxModalClose.onclick = function() {
+    wxModal.style.display = "none";
+}
+
+// rare phraseology
+
+var rpModal = document.getElementById("rpModal");
+var rpbtn = document.getElementById("twrBtnWX");
+var rpModalClose = document.getElementsByClassName("wxModalClose")[0];
+
+rpbtn.onclick = function() {
+    rpModal.style.display = "block";
+}
+
+rpModalClose.onclick = function() {
+    rpModal.style.display = "none";
+}
+
+// phone
+
+var rpModal = document.getElementById("rpModal");
+var rpbtn = document.getElementById("twrBtnWX");
+var rpModalClose = document.getElementsByClassName("wxModalClose")[0];
+
+rpbtn.onclick = function() {
+    rpModal.style.display = "block";
+}
+
+rpModalClose.onclick = function() {
+    rpModal.style.display = "none";
+}
+
+// freqs
+
+// equipment
+
+// maps
+
+// charts
+
+// notams
+
+// pipelines
+
+// misc
+
+// lahso
+
+// approaches
+
+// loa
+
+// wake
+
+// distance
+
+// stars quick ref
+
+// flow
+
+// database
+
+// dep
+
+// lw
+
+// rvr
+
+// home
+
+// sat wx
+
+// sec
+
+// airspace
+
+// relief
+
+// runway info
+
+// visual separation
+
+// traffic altitude alert
+
+// handoff pointout
+
+// laser event
+
+// weather phraseology
+
+// traffic phraseology
+
+// emergency response
