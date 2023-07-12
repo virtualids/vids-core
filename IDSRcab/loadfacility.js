f647045783bb0e4eb4d52332802d40b11ca9231b
Brody Carty <brodycarty@gmail.com>
2023-07-12 12:08:31 -0500

    major changes

---
+++ IDSRcab/loadfacility.js
@@ -0,0 +1,1 @@
+// const facility = <tbd>
---
+++ IDSRcab/modalcontrol.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("settingsModal");
+
+var btn = document.getElementById("twrBtnSettings");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+}
