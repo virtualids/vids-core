37358ec8510ea1cc5cfa1c3cf878281d51797b45
BroDawgYT <brody.durrett@gmail.com>
2023-07-14 20:00:08 -0500

    added modals: TODO-finish html and modalcontrol

---
+++ facilitydatanew/modalcontrol.js
@@ -0,0 +1,42 @@
+
+// zab
+
+var zabModal = document.getElementById("zabModal");
+var zab = document.getElementById("zab");
+var zabModalClose = document.getElementsByClassName("zabModalClose")[0];
+
+zab.onclick = function() {
+    zabModal.style.display = "block";
+}
+
+zabModalClose.onclick = function() {
+    zabModal.style.display = "none";
+}
+
+// zan
+
+var zanModal = document.getElementById("zanModal");
+var zan = document.getElementById("zan");
+var zanModalClose = document.getElementsByClassName("zanModalClose")[0];
+
+zan.onclick = function() {
+    zanModal.style.display = "block";
+}
+
+zanModalClose.onclick = function() {
+    zanModal.style.display = "none";
+}
+
+// ztl
+
+var ztlModal = document.getElementById("ztlModal");
+var ztl = document.getElementById("ztl");
+var ztlModalClose = document.getElementsByClassName("ztlModalClose")[0];
+
+ztl.onclick = function() {
+    ztlModal.style.display = "block";
+}
+
+ztlModalClose.onclick = function() {
+    ztlModal.style.display = "none";
+}
