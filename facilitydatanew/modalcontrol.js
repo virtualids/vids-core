87ab7a452674b3d7cc2553e97bec26ab07e7eb91
BroDawgYT <brody.durrett@gmail.com>
2023-07-14 22:33:08 -0500

    more modals - 3 rows to go

--- facilitydatanew/modalcontrol.js
+++ facilitydatanew/modalcontrol.js
@@ -40,3 +40,129 @@ ztl.onclick = function() {
 ztlModalClose.onclick = function() {
     ztlModal.style.display = "none";
 }
+
+// zbw
+
+var zbwModal = document.getElementById("zbwModal");
+var zbw = document.getElementById("zbw");
+var zbwModalClose = document.getElementsByClassName("zbwModalClose")[0];
+
+zbw.onclick = function() {
+    zbwModal.style.display = "block";
+}
+
+zbwModalClose.onclick = function() {
+    zbwModal.style.display = "none";
+}
+
+// zau
+
+var zauModal = document.getElementById("zauModal");
+var zau = document.getElementById("zau");
+var zauModalClose = document.getElementsByClassName("zauModalClose")[0];
+
+zau.onclick = function() {
+    zauModal.style.display = "block";
+}
+
+zauModalClose.onclick = function() {
+    zauModal.style.display = "none";
+}
+
+// zob
+
+var zobModal = document.getElementById("zobModal");
+var zob = document.getElementById("zob");
+var zobModalClose = document.getElementsByClassName("zobModalClose")[0];
+
+zob.onclick = function() {
+    zobModal.style.display = "block";
+}
+
+zobModalClose.onclick = function() {
+    zobModal.style.display = "none";
+}
+
+// zdv
+
+var zdvModal = document.getElementById("zdvModal");
+var zdv = document.getElementById("zdv");
+var zdvModalClose = document.getElementsByClassName("zdvModalClose")[0];
+
+zdv.onclick = function() {
+    zdvModal.style.display = "block";
+}
+
+zdvModalClose.onclick = function() {
+    zdvModal.style.display = "none";
+}
+
+// zfw
+
+var zfwModal = document.getElementById("zfwModal");
+var zfw = document.getElementById("zfw");
+var zfwModalClose = document.getElementsByClassName("zfwModalClose")[0];
+
+zfw.onclick = function() {
+    zfwModal.style.display = "block";
+}
+
+zfwModalClose.onclick = function() {
+    zfwModal.style.display = "none";
+}
+
+// hcf
+
+var hcfModal = document.getElementById("hcfModal");
+var hcf = document.getElementById("hcf");
+var hcfModalClose = document.getElementsByClassName("hcfModalClose")[0];
+
+hcf.onclick = function() {
+    hcfModal.style.display = "block";
+}
+
+hcfModalClose.onclick = function() {
+    hcfModal.style.display = "none";
+}
+
+// zhu
+
+var zhuModal = document.getElementById("zhuModal");
+var zhu = document.getElementById("zhu");
+var zhuModalClose = document.getElementsByClassName("zhuModalClose")[0];
+
+zhu.onclick = function() {
+    zhuModal.style.display = "block";
+}
+
+zhuModalClose.onclick = function() {
+    zhuModal.style.display = "none";
+}
+
+// zid
+
+var zidModal = document.getElementById("zidModal");
+var zid = document.getElementById("zid");
+var zidModalClose = document.getElementsByClassName("zidModalClose")[0];
+
+zid.onclick = function() {
+    zidModal.style.display = "block";
+}
+
+zidModalClose.onclick = function() {
+    zidModal.style.display = "none";
+}
+
+// zjx
+
+var zjxModal = document.getElementById("zjxModal");
+var zjx = document.getElementById("zjx");
+var zjxModalClose = document.getElementsByClassName("zjxModalClose")[0];
+
+zjx.onclick = function() {
+    zjxModal.style.display = "block";
+}
+
+zjxModalClose.onclick = function() {
+    zjxModal.style.display = "none";
+}
