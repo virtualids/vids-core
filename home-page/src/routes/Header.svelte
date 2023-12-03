85f668eccea47090776a6249c03705aca8a7c80f
bordolbg <brodycarty@gmail.com>
2023-12-02 21:39:38 -0600

    changed the background. looks sick af now

--- home-page/src/routes/Header.svelte
+++ home-page/src/routes/Header.svelte
@@ -1,7 +1,5 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
 </script>
 
 <header>
@@ -10,9 +8,6 @@
 	</div>
 
 	<nav>
-		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg> -->
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">home</a>
@@ -33,9 +28,6 @@
 				<a href="/admin">admin</a>
 			</li>
 		</ul>
-		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg> -->
 	</nav>
 
 	<div class="corner">
