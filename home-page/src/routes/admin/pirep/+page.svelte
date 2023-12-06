2be7cdd8d89e129b951bd79317c2cfe5b55c28bd
jessie846 <104696056+jessie846@users.noreply.github.com>
2023-12-06 15:11:19 +0000

    work on the pirep admin page, work on pirep json data.

--- home-page/src/routes/admin/pirep/+page.svelte
+++ home-page/src/routes/admin/pirep/+page.svelte
@@ -1,2 +1,21 @@
-]<h1>this is a work in progress</h1>
+<svelte:head>
+  <title>Admin</title>
+  <meta name="admin" content="About this app" />
+</svelte:head>
+
+<body class = "pirep">
+<h1>pirep data admin</h1>
+<p>map of reported altitude:</p>
+</body>
+
+<style>
+body {
+  color: light blue; 
+}
+
+
+</style>
+
+
+
 
